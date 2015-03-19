61a4cb43918499d560cd6aa3830ebfbdb82ceaf3
Olivier Gagnon <oli.gagnon4418@gmail.com>
2015-03-19 00:49:33 -0400

    missed one

--- models.go
+++ models.go
@@ -3,7 +3,7 @@ package lux
 import (
 	"github.com/go-gl/gl/v3.3-core/gl"
 	glm "github.com/go-gl/mathgl/mgl32"
-	"github.com/luxengine/lux/utils"
+	"luxengine.net/lux/utils"
 )
 
 type Mesh interface {
