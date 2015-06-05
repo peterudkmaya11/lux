cf9a9e5ffd15ba046cd4ec4faf141208b0b46350
Olivier Gagnon <oli.gagnon4418@gmail.com>
2015-06-05 12:32:41 -0400

    figuring out ways to test main thread only stuff

--- utils_test.go
+++ utils_test.go
@@ -2,32 +2,52 @@ package lux
 
 import (
 	"errors"
-	"fmt"
 	"github.com/go-gl/gl/v3.3-core/gl"
 	"runtime"
 	"testing"
-	"time"
 )
 
-var testchan chan func()
-var donechan chan struct{}
+var testchan chan MainThreadTest
+
+const (
+	mustnotglerror = 0
+)
+
+type MainThreadTest struct {
+	T  *testing.T
+	ID int
+}
 
 func TestMain(m *testing.M) {
 	runtime.LockOSThread()
-	testchan = make(chan func())
-	donechan = make(chan struct{})
+	testchan = make(chan MainThreadTest)
+	//this needs to happen in main thread
 	InitGLFW()
 	_ = StartHeadless()
-	go m.Run()
-	select {
-	case test := <-testchan:
-		test()
-		donechan <- struct{}{}
-	case <-time.After(5 * time.Second):
-		break
+	go func() {
+		m.Run()
+		close(testchan)
+	}()
+	for t := range testchan {
+		switch t.ID {
+		case mustnotglerror:
+			testMustNotGLError(t.T)
+		}
 	}
 	MustNotGLError()
 	TerminateGLFW()
+}
+
+func TestMustNotGLError(t *testing.T) {
+	testchan <- MainThreadTest{
+		T:  t,
+		ID: mustnotglerror,
+	}
+}
+
+func testMustNotGLError(t *testing.T) {
+	gl.Enable(gl.DEPTH_TEST)
+	MustNotGLError()
 }
 
 func TestGLerrorString(t *testing.T) {
@@ -58,14 +78,4 @@ func TestGLerrorString(t *testing.T) {
 	if GLErrorToString(1353) != "Unknown Error Code" {
 		t.Error(errors.New("error trying to get unknown error to string"))
 	}
-}
-
-func TestMustNotGLError(t *testing.T) {
-	testchan <- func() {
-		gl.Enable(gl.DEPTH_TEST)
-		if gl.GetError() != gl.NO_ERROR {
-			t.Fail()
-		}
-	}
-	<-donechan
 }
